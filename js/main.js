a32acec4feadf199691d0dec12883cc8392d02f6
chaluny <21129625+chaluny@users.noreply.github.com>
2023-10-01 23:13:58 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 // Initialize the map
-var map = L.map('map').setView([0, 0], 2); // Set initial map coordinates and zoom level
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+var map = L.map('map').setView([51.505, -0.09], 13); // Set initial map coordinates and zoom level
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 // Sample place data (you can load this from a JSON file or an API)
 var places = [
     {
         name: 'Place 1',
-        lat: 40.7128,
-        lng: -74.0060,
+        lat: 51.5,
+        lng: -0.092,
     },
     {
         name: 'Place 2',
-        lat: 34.0522,
-        lng: -118.2437,
+        lat: 51.7,
+        lng: -0.098,
     },
     // Add more places here
 ];
