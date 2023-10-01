238ce359a4a185d282392000c27a1d4e6540f1a2
chaluny <21129625+chaluny@users.noreply.github.com>
2023-10-02 00:26:43 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Initialize the map
-var map = L.map('map').setView([51.505, -0.09], 5); // Set initial map coordinates and zoom level
+var map = L.map('map').setView([51.505, -0.09], 8); // Set initial map coordinates and zoom level
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
